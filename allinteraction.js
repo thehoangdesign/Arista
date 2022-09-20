09eb6b618df545e8136c67ca02b94f43540769fd
thehoangdev <hoangtt20@uef.edu.vn>
2022-09-20 07:42:56 +0700

    new2

--- allinteraction.js
+++ allinteraction.js
@@ -49,11 +49,11 @@ accordionItemHeaders.forEach(accordionItemHeader => {
     
     // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
     
-    const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-    if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-      currentlyActiveAccordionItemHeader.classList.toggle("active");
-      currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-    }
+    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    // }
 
     accordionItemHeader.classList.toggle("active");
     const accordionItemBody = accordionItemHeader.nextElementSibling;
